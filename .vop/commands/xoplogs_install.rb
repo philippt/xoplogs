fccd0805b80857cf5fc77950467325e2b25e630e
root <root@vopdev20120811.(none)>
2012-10-20 21:07:24 +0200

    installation workaround for mysql_password

--- .vop/commands/xoplogs_install.rb
+++ .vop/commands/xoplogs_install.rb
@@ -5,6 +5,13 @@ param! "domain", "the domain at which the service should be available"
 param! "service_root"
 
 on_machine do |machine, params|
+  begin
+    # TODO same code as in my_sql_install - this version works, though (cause by now the service has been started.)
+    new_password = 'the_password'
+    machine.ssh_and_check_result("command" => "mysqladmin -u root password #{new_password}")
+  rescue
+  end
+  
   [
     'bundle install',
     'rake db:create',
