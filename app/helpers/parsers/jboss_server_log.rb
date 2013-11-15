7942292de711c26450d533c4fdcb45dcd9467872
Philipp T <philipp@virtualop.org>
2013-11-15 16:16:31 +0100

    utc offset for jboss server log

--- app/helpers/parsers/jboss_server_log.rb
+++ app/helpers/parsers/jboss_server_log.rb
@@ -10,13 +10,13 @@ class JbossServerLog
     # 2012-10-22 00:00:30,375 DEBUG [com.arjuna.ats.arjuna.logging.arjLogger] (Thread-12) StatusModule: first pass
     # 2012-10-22 00:00:30,375 DEBUG [com.arjuna.ats.txoj.logging.txojLoggerI18N] (Thread-12) [com.arjuna.ats.internal.txoj.recovery.TORecoveryModule_3] - TORecoveryModule - first pass
     
-              #0              1         2               3           4
-    pattern = /([\d\s:,-]+)\s+(\w+)\s+\[([\w\.]+)\]\s+\(([^)]+)\)\s+(.+)/
+              #0              1         2               3                4
+    pattern = /([\d\s:,-]+)\s+(\w+)\s+\[([\w\.]+)\]\s+(?:\(([^)]+)\)\s+)?(.+)/
     
     matched = pattern.match(line)
     if matched
       {
-        :log_ts => DateTime.parse(matched.captures[0]),
+        :log_ts => Time.at(DateTime.parse(matched.captures[0]).to_i - Time.zone.utc_offset).utc,
         :host_name => @host_name,
         :service_name => @service_name,
         :log_level => matched.captures[1],
